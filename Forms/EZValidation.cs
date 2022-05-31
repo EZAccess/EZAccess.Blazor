b4bc3ff1b2a92b0dfb350f0853758e0ae8a9a5d9
EZ Access Gideon <gideon@ezaccess.nl>
2022-05-31 10:27:33 +0200

    Add project files.

---
+++ Forms/EZValidation.cs
@@ -0,0 +1,54 @@
+﻿namespace TestEZAccessdata.Components;
+
+public class EZValidation : ComponentBase
+{
+    private ValidationMessageStore? messageStore;
+
+    [CascadingParameter]
+    private EditContext? CurrentEditContext { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext is null)
+        {
+            throw new InvalidOperationException(
+                $"{nameof(EZValidation)} requires a cascading " +
+                $"parameter of type {nameof(EditContext)}. " +
+                $"For example, you can use {nameof(EZValidation)} " +
+                $"inside an {nameof(EditForm)}.");
+        }
+
+        messageStore = new(CurrentEditContext);
+
+        CurrentEditContext.OnValidationRequested += (s, e) =>
+            messageStore?.Clear();
+        CurrentEditContext.OnFieldChanged += (s, e) =>
+            messageStore?.Clear(e.FieldIdentifier);
+    }
+
+    public void DisplayErrors(Dictionary<string, List<string>> errors)
+    {
+        if (CurrentEditContext is not null)
+        {
+            foreach (var err in errors)
+            {
+                messageStore?.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+
+    public void ClearErrors()
+    {
+        messageStore?.Clear();
+        CurrentEditContext?.NotifyValidationStateChanged();
+    }
+
+    public void ClearErrors(in FieldIdentifier fieldIdentifier)
+    {
+        messageStore?.Clear(fieldIdentifier);
+        CurrentEditContext?.NotifyValidationStateChanged();
+    }
+
+}
