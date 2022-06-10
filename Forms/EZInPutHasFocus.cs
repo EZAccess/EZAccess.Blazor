b4afd3fd518ce69aec69f76d876fd515eb101c9c
EZ Access Gideon <gideon@ezaccess.nl>
2022-06-10 04:31:14 +0200

    10-6-22

---
+++ Forms/EZInPutHasFocus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace EZAccess.Blazor.Forms;
+
+public class EZInPutHasFocus : INotifyPropertyChanged
+{
+    private ElementReference _input;
+
+    public ElementReference Input
+    {
+        get { return _input; }
+        set { 
+            if (_input.GetHashCode != value.GetHashCode)
+            {
+                _input = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("input"));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
--- usings.cs
+++ usings.cs
@@ -1,3 +1,4 @@
 ﻿global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.Forms;
 global using EZAccess.Data;
